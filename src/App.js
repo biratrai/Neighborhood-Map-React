5474a3f29cca28d1014091c2f060622f186568b4
Birat <birat.rai@gm.com>
2018-09-27 09:49:46 -0400

    changing the text and title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Map from './Map.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,11 +9,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Neighborhood Map</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Map/>
+        </div>
       </div>
     );
   }
