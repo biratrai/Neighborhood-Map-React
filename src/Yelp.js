d656916311ae2afdfa858e95511bdca016eb26cb
Birat Rai <biratrai@hotmail.com>
2018-09-27 21:19:05 -0400

    adding params inline

--- src/Yelp.js
+++ src/Yelp.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const config = {
   headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
     'Authorization': 'Bearer gr2wwg-XsG_mpTRK3PcgsHPaPPmeSa-r9JKLq3HKJSIJ5hvojQsZjxhgqJos4NJND6Tu9LcKXeoAwRdnu3VBX6f5Shs06FuRMKCM13UD1pFXIF8roc_Ypzy8LWqtW3Yx',
     'Access-Control-Allow-Origin': '*',
     "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
@@ -17,7 +19,12 @@ const config = {
 class Yelp extends Component {
 
   componentWillMount() {
-    axios.get('https://api.yelp.com/v3/businesses/search', config)
+    axios.get('https://api.yelp.com/v3/businesses/search', {
+      params: {
+        term: 'restaurant',
+        location: 'detroit'
+      }
+    })
     .then(response => console.log(response))
     .catch(function(error){
       console.log(error);
