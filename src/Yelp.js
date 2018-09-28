2f3c44aac01b15b8faf80435343e2d60a3eeb09b
Birat Rai <biratrai@hotmail.com>
2018-09-27 20:29:40 -0400

    adding Yelp compoent to fetch yelp api

---
+++ src/Yelp.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const config = {
+  headers: {
+    'Authorization': 'Bearer gr2wwg-XsG_mpTRK3PcgsHPaPPmeSa-r9JKLq3HKJSIJ5hvojQsZjxhgqJos4NJND6Tu9LcKXeoAwRdnu3VBX6f5Shs06FuRMKCM13UD1pFXIF8roc_Ypzy8LWqtW3Yx',
+    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+    'crossorigin' : 'true'
+  },
+  params: {
+    term: 'restaurant',
+    location: 'detroit'
+  }
+};
+
+class Yelp extends Component {
+
+  componentWillMount() {
+    axios.get('https://api.yelp.com/v3/businesses/search', config)
+    .then(response => console.log(response))
+    .catch(function(error){
+      console.log(error);
+    });
+    }
+  
+  render(){
+    return (
+        <h1> My first yelp authentication request </h1>
+    );
+  }
+}
+
+export default Yelp;
