6d51281b124cfea7c6788b278645ac118ef42c59
Birat Rai <biratrai@hotmail.com>
2018-09-27 21:52:50 -0400

    using the foursquare api instead of yelp

--- src/Yelp.js
+++ src/Yelp.js
@@ -3,19 +3,23 @@ import axios from 'axios';
 
 const config = {
   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer gr2wwg-XsG_mpTRK3PcgsHPaPPmeSa-r9JKLq3HKJSIJ5hvojQsZjxhgqJos4NJND6Tu9LcKXeoAwRdnu3VBX6f5Shs06FuRMKCM13UD1pFXIF8roc_Ypzy8LWqtW3Yx'
+    'Content-Type': 'application/json'
+    // 'Authorization': 'Bearer gr2wwg-XsG_mpTRK3PcgsHPaPPmeSa-r9JKLq3HKJSIJ5hvojQsZjxhgqJos4NJND6Tu9LcKXeoAwRdnu3VBX6f5Shs06FuRMKCM13UD1pFXIF8roc_Ypzy8LWqtW3Yx'
   },
   params: {
-    term: 'restaurant',
-    location: 'detroit'
+    query: 'restaurant',
+    near: 'detroit',
+    limit: 10,
+    client_id: 'HBYEVO4EIPOICOXSJNABR3PVXDNTCUC4FSM220H1CEVHYGID',
+    client_secret : 'BHNP3EE1OS4TTJTD5HFOQHI4EOQCTNIGA01QJJYBTKZ0NIDH',
+    v : '21120609'
   }
 };
 
 class Yelp extends Component {
 
   componentWillMount() {
-    axios.get('https://api.yelp.com/v3/businesses/search', config)
+    axios.get('https://api.foursquare.com/v2/venues/search', config)
     .then(response => console.log(response))
     .catch(function(error){
       console.log(error);
@@ -28,5 +32,5 @@ class Yelp extends Component {
     );
   }
 }
-
+// https://api.foursquare.com/v2/venues/explore?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee'
 export default Yelp;
