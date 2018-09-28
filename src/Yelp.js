b87c97b0c539d0bbdff18803a49b702c291641a1
Birat Rai <biratrai@hotmail.com>
2018-09-27 21:31:00 -0400

    testing the config

--- src/Yelp.js
+++ src/Yelp.js
@@ -3,12 +3,8 @@ import axios from 'axios';
 
 const config = {
   headers: {
-    'Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer gr2wwg-XsG_mpTRK3PcgsHPaPPmeSa-r9JKLq3HKJSIJ5hvojQsZjxhgqJos4NJND6Tu9LcKXeoAwRdnu3VBX6f5Shs06FuRMKCM13UD1pFXIF8roc_Ypzy8LWqtW3Yx',
-    'Access-Control-Allow-Origin': '*',
-    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-    'crossorigin' : 'true'
+    'Authorization': 'Bearer gr2wwg-XsG_mpTRK3PcgsHPaPPmeSa-r9JKLq3HKJSIJ5hvojQsZjxhgqJos4NJND6Tu9LcKXeoAwRdnu3VBX6f5Shs06FuRMKCM13UD1pFXIF8roc_Ypzy8LWqtW3Yx'
   },
   params: {
     term: 'restaurant',
