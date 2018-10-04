e8abcca5ebcaa91c1ace81db2582820c56dc764e
Birat <biratrai@hotmail.com>
2018-10-04 12:30:56 -0400

    iterating the locationList to update the marker

--- src/Map.js
+++ src/Map.js
@@ -3,28 +3,27 @@ import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 class Map extends Component {
   renderMarker(location) {
-    console.log('location renderMarker '+ location.lat)
+    console.log('location renderMarker '+ location.lat +" "+ location.lng)
     return( 
       <Marker
-      position={{ lat: location.lat, lng: location.lng }}
+      position={{ lat: location.lat , lng: location.lng }}
     />);
   }
 
   render() {
     const GoogleMapExample = withGoogleMap(props => (
+      
        <GoogleMap
          defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
          defaultZoom = { 13 }
         >
-        const locationList = this.props.locationList;
-        console.log("locationList -> "+ locationList)
-        locationList.map((venues) => 
-          {/* this.renderMarker(venues.location); */}
-          console.log('location renderMarker '+ venues.location.lat)
-          )
-        <Marker
-          position={{ lat: 40.756795, lng: -73.954298 }}
-        />
+        {
+          this.props.locationList.map((venues) => {
+            if(venues != null){
+              return this.renderMarker(venues.location);
+            }
+          })
+        }
        </GoogleMap>
     ));
 
