c3250634d9aef4ba27e32af6affd8926514659c5
Birat Rai <biratrai@hotmail.com>
2018-10-03 23:05:51 -0400

    adding renderMarker method

--- src/Map.js
+++ src/Map.js
@@ -2,19 +2,32 @@ import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
 class Map extends Component {
-  render() {
+  renderMarker(location) {
+    console.log('location renderMarker '+ location.lat)
+    return( 
+      <Marker
+      position={{ lat: location.lat, lng: location.lng }}
+    />);
+  }
 
+  render() {
     const GoogleMapExample = withGoogleMap(props => (
        <GoogleMap
          defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
          defaultZoom = { 13 }
         >
+        const locationList = this.props.locationList;
+        console.log("locationList -> "+ locationList)
+        locationList.map((venues) => 
+          {/* this.renderMarker(venues.location); */}
+          console.log('location renderMarker '+ venues.location.lat)
+          )
         <Marker
           position={{ lat: 40.756795, lng: -73.954298 }}
         />
        </GoogleMap>
     ));
- 
+
     return(
        <div>
          <GoogleMapExample
