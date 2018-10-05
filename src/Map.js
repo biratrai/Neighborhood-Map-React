23aa5b2e72a05bbdbfad6c42560f2d712d22e380
Birat <biratrai@hotmail.com>
2018-10-05 15:42:12 -0400

    cleaning the mess of lines since we moved marker logic to it's separate js class

--- src/Map.js
+++ src/Map.js
@@ -1,75 +1,14 @@
 import React, { Component } from 'react';
-import { withGoogleMap, withScriptjs,GoogleMap, Marker, InfoWindow } from 'react-google-maps';
-const { compose, withProps, withStateHandlers } = require("recompose");
+import { withGoogleMap,GoogleMap } from 'react-google-maps';
+import MarkerWithInfoWindow from './MarkerWithInfoWindow'
 
 class Map extends Component {
   renderMarker(venue) {
     console.log('location renderMarker '+ venue.location.lat +" "+ venue.location.lng)
-    var isOpen = false;
-    var onToggleOpen= (isOpen) => {
-      console.log("onToggleOpen")
-      this.isOpen = !!isOpen
-    }
     return(
-      
-      <Marker key={venue.location.address}
-      position={{ lat: venue.location.lat , lng: venue.location.lng }}
-      onClick={()=>{onToggleOpen(true)}}
-      >
-        {isOpen && <InfoWindow>
-          <div onCloseclick={onToggleOpen(false)}>
-            {/* <img src="https://i.imgur.com/7IgBxnH.jpg" width="150px" /> */}
-            <div>{venue.name}</div>
-            <div>{venue.location.address}</div>
-          </div>
-        </InfoWindow>}
-      </Marker>
+      <MarkerWithInfoWindow venue={venue} key={venue.location.address}/>
     ); 
   }
-
-   
-
-  // renderMarker(venue) {
-  //   const MapWithAMakredInfoWindow = compose(
-  //     withStateHandlers(() => ({
-  //       isOpen: false,
-  //     }), {
-  //       onToggleOpen: ({ isOpen }) => () => ({
-  //         isOpen: !isOpen,
-  //       })
-  //     }),
-  //   )(stateOfProps =>
-    
-  //       <Marker
-  //         position={{ lat: 40.756795, lng: -73.954298}}
-  //         onClick={stateOfProps.onToggleOpen}
-  //       > {
-
-  //       }
-  //         {stateOfProps.isOpen && <InfoWindow onCloseClick={stateOfProps.onToggleOpen}>
-            
-  //         </InfoWindow>}
-  //       </Marker>
-  //   );
-  //   return(
-  //     <MapWithAMakredInfoWindow/>
-  //   );
-  // }
-//   toggleInfoWindow(){
-// <Marker key={venue.location.address}
-//       position={{ lat: venue.location.lat , lng: venue.location.lng }}
-//       onClick={()=>{this.onToggleOpen}}
-//     >
-//       <InfoWindow>
-//         <div onCloseclick={this.onToggleOpen}>
-//           {/* <img src="https://i.imgur.com/7IgBxnH.jpg" width="150px" /> */}
-//           <div>{venue.name}</div>
-//           <div>{venue.location.address}</div>
-//         </div>
-//       </InfoWindow>
-//     </Marker>
-    
-//   }
 
   render() {
     const GoogleMapExample = withGoogleMap(props => (
