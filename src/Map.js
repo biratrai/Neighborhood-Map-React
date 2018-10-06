d80e62290911aacb8e3e0adfb016e327cf6dfe7c
Birat Rai <biratrai@hotmail.com>
2018-10-06 13:33:07 -0400

    removing unnecessary logic

--- src/Map.js
+++ src/Map.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import { withGoogleMap,GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import { withGoogleMap,GoogleMap} from 'react-google-maps';
 import MarkerWithInfoWindow from './MarkerWithInfoWindow'
 
 class Map extends Component {
   renderMarker(venue, isOpen) {
-    console.log('location renderMarker '+ venue.location.lat +" "+ venue.location.lng)
-    // console.log("this.props.isOpen " + this.props.isOpen)
-    // console.log("venue.location.address " + venue.location.address)
     return(
-      // <MarkerWithInfoWindow 
-      //   venue={venue} 
-      //   isOpen = {isOpen}
-      //   onToggleOpen = {onToggleOpen}
-      //   key={venue.location.address}/>
-      <Marker 
-      position={{ lat: venue.location.lat, lng: venue.location.lng }}
-      onClick={ () => this.props.onToggleOpen(venue.location.address)}
-    >
-      { 
-        this.props.isOpen === venue.location.address && <InfoWindow onCloseClick={this.props.onToggleOpen("")}>
-      </InfoWindow>}
-    </Marker>
+      <MarkerWithInfoWindow 
+        venue={venue} 
+        isOpen = {isOpen}
+        onToggleOpen = {this.props.onToggleOpen}
+        key={venue.location.address}/>
     ); 
   }
 
