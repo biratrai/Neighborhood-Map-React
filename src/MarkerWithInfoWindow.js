bb27e8c96dc6ef38cb7f7e77d4f5a49cb108f50a
Birat Rai <biratrai@hotmail.com>
2018-10-06 13:33:17 -0400

    removing unecessary logic

--- src/MarkerWithInfoWindow.js
+++ src/MarkerWithInfoWindow.js
@@ -5,29 +5,22 @@ const {
 } = require("react-google-maps");
 
 class MarkerWithInfoWindow extends Component {
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     isOpen: false,
-  //   }
-  // }
-
-  // onToggleOpen = ( isOpen ) => {
-  //   console.log("onToggleOpen "+isOpen)
-  //   this.setState({
-  //     isOpen : !isOpen
-  //   }); 
-  // }
 
 render(){
+  console.log(this.props.venue.categories[0].icon.prefix+32+this.props.venue.categories[0].icon.suffix)
   return(
-  <Marker 
-    position={{ lat: this.props.venue.location.lat, lng: this.props.venue.location.lng }}
-    onClick={ console.log("onClicked")}
-  >
-    {this.props.isOpen === this.props.venue.location.address && <InfoWindow onCloseClick={this.props.onToggleOpen("")}>
-    </InfoWindow>}
-  </Marker>
+        <Marker 
+      position={{ lat: this.props.venue.location.lat, lng: this.props.venue.location.lng }}
+      onClick={ () => this.props.onToggleOpen(this.props.venue.location.address)}
+    >
+      { 
+        this.props.isOpen === this.props.venue.location.address && <InfoWindow >
+        <div onClick={() => this.props.onToggleOpen("")}>
+          <div>{this.props.venue.name}</div>
+          <div>{this.props.venue.location.address}</div>
+       </div>
+      </InfoWindow>}
+    </Marker>
   );
 }
     
