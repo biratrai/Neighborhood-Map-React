53c689d5d306fa0c43cf3fc5960cf6d3b6b59fa5
Birat Rai <biratrai@hotmail.com>
2018-10-14 10:18:18 -0400

    using firlterItemList as the base

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import FilterItemList from './FilterItemList';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FilterItemList />, document.getElementById('root'));
 registerServiceWorker();
