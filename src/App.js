ce6227f397c12cf304aa7f253c1a992f8b6b519e
Birat Rai <biratrai@hotmail.com>
2018-10-24 22:10:43 -0400

    extracting the api call to parent AppFrame component

--- src/App.js
+++ src/App.js
@@ -1,66 +1,25 @@
 import React, { Component } from 'react';
 import Map from './Map.js';
 import Search from './Search.js'
-import {getLocationList} from './LocationList.js'
 import logo from './logo.svg';
 import Loader from 'react-loader-spinner'
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      locations: [],
-      apiReturned: false,
-      hasError: false,
-      errorMsg: "",
-      isLoading: true
-    }
-  }
-
-  getData = (response) => {
-    if(response.data !== undefined){
-      return this.setLocationData(response);
-    } else {
-      return this.setErrorData(response);
-    }
-  }
-
-  setLocationData = (response) => {
-    this.setState(
-      {
-        locations : response.data.response.venues,
-        apiReturned : true,
-        isLoading: false
-      });
-  }
-
-  setErrorData = (error) => {
-    this.setState(
-      {
-        hasError : true,
-        errorMsg: error,
-        isLoading: false
-      });
-  }
-
-  componentDidMount(){
-    getLocationList(this.getData)
-  }
 
   render() {
     let content;
-    if(this.state.hasError){
-      content = <div className="error" >{this.state.errorMsg}</div>
+    if( this.props.hasError ){
+      content = <div className="error" >{ this.props.errorMsg }</div>
     } else {
       content = <div>
         <section>
           <Search 
-            locationList= { this.state.locations }
-            apiReturned = { this.state.apiReturned }
+            locationList= { this.props.locations }
+            apiReturned = { this.props.apiReturned }
           />
         </section>
-        <Map locationList = {this.state.locations}/>
+        <Map locationList = { this.props.locations }/>
       </div>
     }
     return (
@@ -69,14 +28,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Neighborhood Map</h1>
         </header>
-        {this.state.isLoading &&  <Loader 
+        { this.props.isLoading &&  <Loader 
             type="Circles"
             color="#00BFFF"
             height="200"	
             width="200"
           />   
         }
-        {content}
+        { content }
       </div>
     );
   }
