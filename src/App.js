0c86f1ae62fa2477ea577015298c5d87031b8fd9
Birat Rai <biratrai@hotmail.com>
2018-10-25 21:58:52 -0400

    removing unused import and removing commented code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Map from './Map.js';
-import Search from './Search.js'
-import logo from './logo.svg';
 import Loader from 'react-loader-spinner'
 import './App.css';
 
@@ -13,21 +11,11 @@ class App extends Component {
       content = <div className="error" >{ this.props.errorMsg }</div>
     } else {
       content = <div>
-        <section>
-          {/* <Search 
-            locationList= { this.props.locations }
-            apiReturned = { this.props.apiReturned }
-          /> */}
-        </section>
         <Map locationList = { this.props.locations }/>
       </div>
     }
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Neighborhood Map</h1>
-        </header> */}
         { this.props.isLoading &&  <Loader 
             type="Circles"
             color="#00BFFF"
