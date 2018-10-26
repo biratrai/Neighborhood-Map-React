b802a8aa505108c704e54e878c2154a6e9c9faf6
Birat Rai <biratrai@hotmail.com>
2018-10-25 21:56:59 -0400

    adding method to get the change from input and reverting to class as event couldn't be handled

--- src/FilterItemList.js
+++ src/FilterItemList.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import TextField from '@material-ui/core/TextField';
 import List from '@material-ui/core/List';
 
-// Stateless functional Component
-const FilterItemList = ({ locations } ) => { 
-    return (
-        <List>
-            <ListItem button>
-                <TextField
-                    id="standard-search"
-                    label="Filter locations"
-                    type="search"
-                    />
-            </ListItem>    
-            { locations.map((venue) =>
-                { 
-                    return <ListItem button onClick={alert}>
-                                <ListItemText primary={venue.name} />
-                            </ListItem>
-                }
-            )}
-        </List>
-    );
+class FilterItemList extends Component {
+    
+    handleChange = filterLocation => event => {
+        filterLocation(event.target.value)
+    };
+
+    render(){
+        return (
+            <List>
+                <ListItem button>
+                    <TextField
+                        id="standard-search"
+                        label="Filter locations"
+                        type="search"
+                        onChange={ this.handleChange(this.props.filterLocation) }
+                        />
+                </ListItem>    
+                { this.props.locations.map((venue) =>
+                    { 
+                        return <ListItem button onClick={alert}>
+                                    <ListItemText primary={venue.name} />
+                                </ListItem>
+                    }
+                )}
+            </List>
+        );
+    }
 }
 
 export default FilterItemList;
