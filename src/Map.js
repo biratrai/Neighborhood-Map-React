895f506999a1a201c6a7e45cc08104893be2192f
Birat Rai <biratrai@hotmail.com>
2018-10-28 13:22:42 -0400

    Extracting the GoogleMap component so that it doesn't refresh automatically

--- src/Map.js
+++ src/Map.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import { withGoogleMap,GoogleMap} from 'react-google-maps';
 import MarkerWithInfoWindow from './MarkerWithInfoWindow'
 
+const GoogleMapBox = withGoogleMap(props => (
+      
+  <GoogleMap
+    defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
+    defaultZoom = { 13 }
+   >
+   {
+     props.locationList.map((venue) => {
+       if(venue != null){
+         return props.renderMarker(venue, props.shouldAnimate, props.currentSelected, props.isOpen, props.onToggleOpen);
+       } else {
+         return null;
+       }
+     })
+   }
+  </GoogleMap>
+));
+
 class Map extends Component {
   constructor(props){
     super(props);
@@ -16,12 +34,12 @@ class Map extends Component {
     }); 
   }
 
-  renderMarker(venue, shouldAnimate, currentSelected) {
+  renderMarker(venue, shouldAnimate, currentSelected, isOpen, onToggleOpen ) {
     return(
       <MarkerWithInfoWindow 
         venue={ venue } 
-        isOpen = { this.state.isOpen }
-        onToggleOpen = { this.onToggleOpen }
+        isOpen = { isOpen }
+        onToggleOpen = { onToggleOpen }
         key={ venue.location.address }
         shouldAnimate={ shouldAnimate }
         animation={ window.google.maps.Animation.BOUNCE }
@@ -30,29 +48,17 @@ class Map extends Component {
   }
 
   render() {
-    const GoogleMapBox = withGoogleMap(props => (
-      
-       <GoogleMap
-         defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
-         defaultZoom = { 13 }
-        >
-        {
-          this.props.locationList.map((venue) => {
-            if(venue != null){
-              return this.renderMarker(venue, this.props.shouldAnimate, this.props.currentSelected);
-            } else {
-              return null;
-            }
-          })
-        }
-       </GoogleMap>
-    ));
-
     return(
        <div>
          <GoogleMapBox
            containerElement={ <div style={{ height: `500px`}} /> }
            mapElement={ <div style={{ height: `100%` }} /> }
+           locationList={ this.props.locationList }
+           shouldAnimate={ this.props.shouldAnimate }
+           currentSelected={ this.props.currentSelected }
+           renderMarker={ this.renderMarker }
+           isOpen={ this.state.isOpen }
+           onToggleOpen={ this.onToggleOpen }
          >
 
          </GoogleMapBox>
