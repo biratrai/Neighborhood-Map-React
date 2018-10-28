28c11ee7f75c46ebb71f23c621d8c226b6442463
Birat Rai <biratrai@hotmail.com>
2018-10-28 16:24:06 -0400

    passing id when onToggleOpen and formating

--- src/MarkerWithInfoWindow.js
+++ src/MarkerWithInfoWindow.js
@@ -8,14 +8,15 @@ class MarkerWithInfoWindow extends Component {
 
 render(){
   // console.log(this.props.venue.categories[0].icon.prefix+32+this.props.venue.categories[0].icon.suffix)
+  
   return(
     <Marker 
       position={{ lat: this.props.venue.location.lat, lng: this.props.venue.location.lng }}
-      onClick={ () => this.props.onToggleOpen(this.props.venue.location.address)}
+      onClick={ () => this.props.onToggleOpen(this.props.venue.id)}
       animation= { this.props.currentSelected === this.props.venue.id && this.props.shouldAnimate? this.props.animation: null }
     >
       { 
-        this.props.isOpen === this.props.venue.location.address && <InfoWindow >
+        (this.props.isOpen === this.props.venue.id) && <InfoWindow >
         <div onClick={() => this.props.onToggleOpen("")}>
           <h3>{this.props.venue.name}</h3>
           <div>{this.props.venue.location.address}</div>
