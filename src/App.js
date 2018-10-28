7f45b20b4cae640ae910c3112945ac77a6ea40a8
Birat Rai <biratrai@hotmail.com>
2018-10-28 16:07:31 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
       content = <div className="error" >{ this.props.errorMsg }</div>
     } else {
       content = <div>
-        <Map locationList = { this.props.locations } shouldAnimate = { this.props.shouldAnimate } currentSelected={ this.props.currentSelected }/>
+        <Map locationList = { this.props.locations } 
+          shouldAnimate = { this.props.shouldAnimate } 
+          currentSelected={ this.props.currentSelected }/>
       </div>
     }
     return (
