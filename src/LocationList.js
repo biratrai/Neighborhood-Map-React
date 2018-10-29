5be1f2b88f5bf3d7ecdc61a9b30e6a47c164d81f
Birat Rai <biratrai@hotmail.com>
2018-10-28 23:09:40 -0400

    adding default value for near

--- src/LocationList.js
+++ src/LocationList.js
@@ -6,7 +6,7 @@ const config = {
   },
   params: {
     query: 'restaurant',
-    near: 'manhattan',
+    near: "manhattan",
     limit: 10,
     client_id: 'HBYEVO4EIPOICOXSJNABR3PVXDNTCUC4FSM220H1CEVHYGID',
     client_secret : 'BHNP3EE1OS4TTJTD5HFOQHI4EOQCTNIGA01QJJYBTKZ0NIDH',
@@ -14,7 +14,8 @@ const config = {
   }
 };
 
-export const getLocationList = (getData) => {
+export const getLocationList = (getData, location) => {
+  config.params.near = location;
   axios.get('https://api.foursquare.com/v2/venues/search', config)
   .then(response => {
     console.log(response);
