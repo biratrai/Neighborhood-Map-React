47f370e53cfffc31889afd5471a7d235b55a0f8c
Birat Rai <biratrai@hotmail.com>
2018-10-29 20:21:22 -0400

    adding restaurant type to the marker info window

--- src/MarkerWithInfoWindow.js
+++ src/MarkerWithInfoWindow.js
@@ -8,7 +8,7 @@ class MarkerWithInfoWindow extends Component {
 
 render(){
   // console.log(this.props.venue.categories[0].icon.prefix+32+this.props.venue.categories[0].icon.suffix)
-  
+  console.log("render "+ this.props.venue.categories.name)
   return(
     <Marker 
       position={{ lat: this.props.venue.location.lat, lng: this.props.venue.location.lng }}
@@ -18,8 +18,9 @@ render(){
       { 
         (this.props.currentSelected === this.props.venue.id) && <InfoWindow >
         <div onClick={() => this.props.onToggleOpen("")}>
-          <h3>{this.props.venue.name}</h3>
-          <div>{this.props.venue.location.address}</div>
+          <h3>{ this.props.venue.name }</h3>
+          <h5>{ this.props.venue.categories[0].name }</h5>
+          <p>{ this.props.venue.location.address }</p>
        </div>
       </InfoWindow>}
     </Marker>
