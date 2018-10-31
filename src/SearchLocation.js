16c816b73d0c2b5f60bd7ec78f8ce18a6c2d49e5
Birat Rai <biratrai@hotmail.com>
2018-10-30 21:27:14 -0400

    making it stateless functional component and adding prop-types

--- src/SearchLocation.js
+++ src/SearchLocation.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ListItem from '@material-ui/core/ListItem';
 import List from '@material-ui/core/List';
 import { DebounceInput } from 'react-debounce-input';
 
-class SearchLocation extends Component {
-    
-    handleChange = searchLocation => event => {
-        console.log("event "+event.target.value)
+// Stateless functional Component
+const SearchLocation = ({ searchLocation }) => {
+    const handleChange = searchLocation => event => {
         if(event.target.value)
             searchLocation(event.target.value)
     };
-
-    render(){
-        return (
-            <List>
-                <ListItem button>
-                    <DebounceInput
-                        minLength= { 2 }
-                        debounceTimeout={ 400 }
-                        placeholder="Search by city name"
-                        element="input" 
-                        style={{flex: "1", fontSize: "1.25em"}}
-                        onChange={ this.handleChange(this.props.searchLocation) }
-                        />
-                </ListItem>    
-            </List>
-        );
-    }
+    return (
+        <List>
+            <ListItem button>
+                <DebounceInput
+                    minLength= { 2 }
+                    debounceTimeout={ 400 }
+                    placeholder="Search by city name"
+                    element="input" 
+                    style={{flex: "1", fontSize: "1.25em"}}
+                    onChange={ handleChange(searchLocation) }
+                    />
+            </ListItem>    
+        </List>
+    );
 }
 
+SearchLocation.propTypes = {
+    searchLocation: PropTypes.func.isRequired
+};
 export default SearchLocation;
