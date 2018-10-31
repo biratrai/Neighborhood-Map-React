5e5df936ff61a2431e5a60fafa729b5660575562
Birat Rai <biratrai@hotmail.com>
2018-10-30 21:48:19 -0400

    formatting and makin it // Stateless functional Component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,69 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Map from './Map.js';
 import './App.css';
 import Typography from '@material-ui/core/Typography';
 import { ClipLoader } from 'react-spinners';
 import { css } from 'react-emotion';
 
-const override = css`
-    display: block;
-    margin: 0 auto;
-    border-color: red;
-`;
-class App extends Component {
-
-  render() {
+// Stateless functional Component
+const App = (props) => {
     let content;
-    if( this.props.hasError ){
-      if (this.props.errorMsg.response) {
+    if(props.hasError ){
+      // If error show appropriate error
+      if (props.errorMsg.response) {
         content = <div className="error" >
-        <h3>{ this.props.errorMsg.message }</h3>
-          <p>{ this.props.errorMsg.response.data.meta.errorDetail }</p> 
+        <h3>{ props.errorMsg.message }</h3>
+          <p>{ props.errorMsg.response.data.meta.errorDetail }</p> 
           <p>{ "Please search again!" }</p>
         </div>
-      } else if (this.props.errorMsg.request) {
+      } else if (props.errorMsg.request) {
         content = <Typography variant="h5" color="error">
                     "Couldn't fetch data. Please try again later"
                   </Typography>
           
       }
+      // If successful show Map
     } else {
       content = <div>
-        <Map locationList = { this.props.locations } 
-          shouldAnimate = { this.props.shouldAnimate } 
-          currentSelected={ this.props.currentSelected }
-          onToggleOpen={ this.props.onToggleOpen }
-          geometry={ this.props.geometry }
+        <Map locationList = { props.locations } 
+          shouldAnimate = { props.shouldAnimate } 
+          currentSelected={ props.currentSelected }
+          onToggleOpen={ props.onToggleOpen }
+          geometry={ props.geometry }
           />
       </div>
     }
+    
+    console.log("loading "+props.isLoading)
     return (
+      // If loading show spinner else load content
       <div className="App">
-        { this.props.isLoading &&  <ClipLoader
+        {props.isLoading &&  <ClipLoader
           className={override}
           sizeUnit={"px"}
           size={80}
           color={'#123abc'}/>
         }
-        { !this.props.isLoading && content } 
+        { !props.isLoading && content } 
       </div>
     );
-  }
 }
 
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
+
+App.propTypes = {
+  hasError: PropTypes.bool.isRequired,
+  errorMsg: PropTypes.string.isRequired,
+  locations: PropTypes.array.isRequired,
+  shouldAnimate: PropTypes.bool.isRequired,
+  currentSelected: PropTypes.string.isRequired,
+  onToggleOpen: PropTypes.func.isRequired,
+  geometry: PropTypes.object.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
 export default App;
