d0ca73e88cb65f8a2284903a81134de857847c7f
Birat Rai <biratrai@hotmail.com>
2018-10-30 22:04:44 -0400

    making it stateless functional component

--- src/MarkerWithInfoWindow.js
+++ src/MarkerWithInfoWindow.js
@@ -1,29 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Stateless functional Component
+const MarkerWithInfoWindow = (props) => {
+  return(
+    <Marker 
+      position={{ lat: props.venue.location.lat, lng: props.venue.location.lng }}
+      onClick={ () => props.onToggleOpen(props.venue.id)}
+      animation= { props.currentSelected === props.venue.id && props.shouldAnimate? props.animation: null }
+    >
+      { 
+        (props.currentSelected === props.venue.id) && <InfoWindow >
+        <div onClick={() => props.onToggleOpen(props.venue.id)}>
+          <h3>{ props.venue.name }</h3>
+          <h5>{ props.venue.categories[0].name }</h5>
+          <p>{ props.venue.location.address }</p>
+       </div>
+      </InfoWindow>}
+    </Marker>
+  );
+};
+
 const {
   Marker,
   InfoWindow,
 } = require("react-google-maps");
 
-class MarkerWithInfoWindow extends Component {
-
-render(){
-  return(
-    <Marker 
-      position={{ lat: this.props.venue.location.lat, lng: this.props.venue.location.lng }}
-      onClick={ () => this.props.onToggleOpen(this.props.venue.id)}
-      animation= { this.props.currentSelected === this.props.venue.id && this.props.shouldAnimate? this.props.animation: null }
-    >
-      { 
-        (this.props.currentSelected === this.props.venue.id) && <InfoWindow >
-        <div onClick={() => this.props.onToggleOpen(this.props.venue.id)}>
-          <h3>{ this.props.venue.name }</h3>
-          <h5>{ this.props.venue.categories[0].name }</h5>
-          <p>{ this.props.venue.location.address }</p>
-       </div>
-      </InfoWindow>}
-    </Marker>
-  );
-}
-    
+MarkerWithInfoWindow.propTypes = {
+  venue: PropTypes.object.isRequired,
+  // isOpen: PropTypes.string.isRequired,
+  onToggleOpen: PropTypes.func.isRequired,
+  shouldAnimate: PropTypes.bool.isRequired,
+  animation: PropTypes.number.isRequired,
 };
 export default MarkerWithInfoWindow;
